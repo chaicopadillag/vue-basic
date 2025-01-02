4ce0bc65290e39af2c4c8bdfb8e14bdec5aa4b35
chaicopadillag <xt9738@rimac.com.pe>
2025-01-02 18:37:23 -0500

    add auto-scrolling feature to chat messages component

--- src/components/chat/ChatMessages.vue
+++ src/components/chat/ChatMessages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 overflow-y-auto p-4">
+  <div ref="refChat" class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
       <template v-for="{ id, message, isReceived, imageUrl } in messages">
         <ReceivedMessage v-if="isReceived" :message="message" :image-url="imageUrl" :key="id" />
@@ -13,6 +13,22 @@
 import type { ChatMessage } from '@/components/chat/interfaces/ChatType';
 import ReceivedMessage from '@/components/chat/messages/ReceivedMessage.vue';
 import SendMessage from '@/components/chat/messages/SendMessage.vue';
+import { ref, watch } from 'vue';
 
-defineProps<{ messages: ChatMessage[] }>();
+type Props = {
+  messages: ChatMessage[];
+};
+
+const props = defineProps<Props>();
+
+const refChat = ref<HTMLDivElement>();
+
+watch(props.messages, () => {
+  setTimeout(() => {
+    refChat.value?.scrollTo({
+      top: refChat.value.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, 100);
+});
 </script>
