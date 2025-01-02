09b4610bc04e4c763d6bdfbc170ed318064fa659
chaicopadillag <xt9738@rimac.com.pe>
2025-01-02 10:57:17 -0500

    initialize Vue 3 + TypeScript + Vite project with essential configurations and files

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Vue 3 + TypeScript + Vite</h1>
+    <p>Start coding your Vue 3 + TypeScript app here!</p>
+  </div>
+  <section>
+    <h2>Counter</h2>
+    <p>{{ count }}</p>
+    <hr />
+    <h2>Square</h2>
+    <p>{{ square }}</p>
+    <button type="button" @click="count++">Increment</button>
+    <button type="button" @click="count--">Decrement</button>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+
+const count = ref(2);
+const square = computed(() => count.value * count.value);
+</script>
